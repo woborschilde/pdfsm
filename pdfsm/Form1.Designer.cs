04099430caf073e3a382383baa764f57dd302d9b
idnaos <riko.woborschil@googlemail.com>
2019-07-20 18:11:36 +0200

    Project files added

---
+++ pdfsm/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace pdfsm {
+    partial class Form1 {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.vistaFolderBrowserDialog1 = new Ookii.Dialogs.VistaFolderBrowserDialog();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(340, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = ".ini Select File:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(465, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "INI Configuration Files|*.ini";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "PDF Document|*.pdf";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(340, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PDF Input Path:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(465, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 169);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(549, 153);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Log:";
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(465, 127);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 36);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Merge!";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(380, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(182, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "v1.0 (20.07.2019), © woborschil.de";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(119, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(340, 23);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = ".pdf Output File:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(465, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Browse...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 325);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Help";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(360, 140);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Open Result";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(188, 331);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(91, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "GitHub Releases";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 353);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF Select & Merge";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private Ookii.Dialogs.VistaFolderBrowserDialog vistaFolderBrowserDialog1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ pdfsm/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+
+namespace pdfsm {
+    public partial class Form1 : Form {
+
+        private string ConfigFile = ".\\pdfsm_config.ini";
+        private string SelectFile = "";
+        private string InputPath = "";
+        private string SaveFile = "";
+        private bool ErrorsOccurred = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        #region Functions
+        /// <summary>Checks if the merge button is available (all input fields have to be filled).</summary>
+        private void MergeButtonAvailable() {
+            if (!textBox1.Text.Equals("") && !textBox2.Text.Equals("") && !textBox3.Text.Equals("")) {
+                button4.Enabled = true;
+            } else {
+                button4.Enabled = false;
+            }
+        }
+
+        /// <summary>Writes entered paths to a config file to remember them next time (if possible).</summary>
+        private void Write(string key, string value) {
+            try {
+                IniFileHelper.WriteValue("General", key, value, ConfigFile);
+            } catch (Exception) {
+                Append("Tip: If you move this program to a writable directory, it could remember your configured paths. ;)\n");
+            }
+        }
+
+        /// <summary>Merges the PDF Documents based on the INI Selector File.</summary>
+        private void Merge() {
+            button6.Enabled = false; richTextBox1.Clear(); ErrorsOccurred = false;
+
+            // Preparation
+            string Description = Read("General", "Description");
+            int InputFilesCount = Convert.ToInt32(Read("General", "InputFilesCount", "0"));
+            string InputFileName = ""; string SelectPages = ""; string[] SelectPagesArray;
+            PdfDocument PdfIn = null; PdfDocument PdfOut = new PdfDocument();
+
+            if (Description != "") {
+                Append(Description + "\n\n");
+            }
+
+            // For each file
+            for (int i = 1; i <= InputFilesCount; i++) {
+                // Preparation
+                Append("Using file " + i + " of " + InputFilesCount + "...\n");
+                InputFileName = Read("Input" + i, "InputFile");
+
+                // Check existance
+                if (File.Exists(InputPath + InputFileName + ".pdf")) {
+                    try {
+                        PdfIn = PdfReader.Open(InputPath + InputFileName + ".pdf", PdfDocumentOpenMode.Import);
+                    } catch (Exception) {
+                        Append("Fixing corrupted file " + InputFileName + ".pdf...\n");
+                        Process proc = Process.Start(".\\gswin32c.exe", "-o \"" + Path.GetDirectoryName(SaveFile) + "\\pdfsm_fixed.pdf\" -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress \"" + InputPath + InputFileName + ".pdf\"");
+                        proc.WaitForExit();
+                        PdfIn = PdfReader.Open(Path.GetDirectoryName(SaveFile) + "\\pdfsm_fixed.pdf", PdfDocumentOpenMode.Import);
+                    }
+                } else {
+                    Append("Skipping file because not found in Input Path:\n" + InputPath + InputFileName + ".pdf\n\n");
+                    continue;
+                }
+
+                // Read pages
+                SelectPages = Read("Input" + i, "SelectPages");
+                SelectPagesArray = SelectPages.Split(',');
+
+                // For each page in file
+                foreach (string p in SelectPagesArray) {
+                    Append("Copying page " + p + "...\n");
+                    CopyPage(PdfIn, PdfOut, Convert.ToInt32(p));  // Actual copy process
+                }
+
+                Append("\n");
+            }
+
+            // Finish
+            if (File.Exists(Path.GetDirectoryName(SaveFile) + "\\pdfsm_fixed.pdf")) {
+                File.Delete(Path.GetDirectoryName(SaveFile) + "\\pdfsm_fixed.pdf");
+            }
+
+            if (PdfOut.PageCount > 0) {
+                try {
+                    PdfOut.Save(SaveFile);  // Actual save process
+                    button6.Enabled = true;
+
+                    if (!ErrorsOccurred) {
+                        Append("Finished.");
+                    } else {
+                        Append("Finished with errors (check the log!).");
+                    }
+                } catch (Exception ex) {
+                    Append("Could not save output file:\n" + ex.Message);
+                }
+            } else {
+                Append("No output file has been saved as it would be empty (either input files missing or selection file wrong).");
+            }
+        }
+
+        /// <summary>Copies a page from a PdfDocument to a PdfDocument.</summary>
+        private void CopyPage(PdfDocument from, PdfDocument to, int pageNumber) {
+            try {
+                to.AddPage(from.Pages[pageNumber - 1]);
+            } catch (Exception ex) {
+                Append("Skipping page " + pageNumber + " (does it exist?):\n" + ex.Message + "\n");
+                ErrorsOccurred = true;
+            }
+        }
+
+        /// <summary>Reads a key from an INI file and returns its value as a string.</summary>
+        public string Read(string section, string key, string defaultValue = "", string file = "") {
+            if (file.Equals("")) { file = SelectFile; };
+            return IniFileHelper.ReadValue(section, key, file, defaultValue);
+        }
+
+        /// <summary>Appends text to the log.</summary>
+        private void Append(String text) {
+            richTextBox1.AppendText(text);
+        }
+        #endregion
+
+        #region Button Events
+        private void button1_Click(object sender, EventArgs e) {
+            string c = Read("General", "SelectFile", "", ConfigFile);
+            if (!c.Equals("")) {
+                openFileDialog1.FileName = Path.GetFileName(c);
+                openFileDialog1.InitialDirectory = Path.GetDirectoryName(c);
+            }
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                SelectFile = openFileDialog1.FileName;
+                textBox1.Text = SelectFile;
+
+                saveFileDialog1.FileName = IniFileHelper.ReadValue("General", "DefaultOutputName", SelectFile, "");
+                MergeButtonAvailable();
+                Write("SelectFile", SelectFile);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            string c = Read("General", "InputPath", "", ConfigFile);
+            if (!c.Equals("")) {
+                vistaFolderBrowserDialog1.SelectedPath = c;
+            }
+
+            if (vistaFolderBrowserDialog1.ShowDialog() == DialogResult.OK) {
+                InputPath = vistaFolderBrowserDialog1.SelectedPath + "\\";
+                textBox2.Text = InputPath;
+                MergeButtonAvailable();
+                Write("InputPath", InputPath);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            string c = Read("General", "SaveFile", "", ConfigFile);
+            if (!c.Equals("")) {
+                saveFileDialog1.FileName = Path.GetFileName(c);
+                saveFileDialog1.InitialDirectory = Path.GetDirectoryName(c);
+
+                string k = Read("General", "DefaultOutputName");
+                if (!k.Equals("")) {
+                    saveFileDialog1.FileName = k;
+                }
+            }
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                SaveFile = saveFileDialog1.FileName;
+                textBox3.Text = SaveFile;
+                MergeButtonAvailable();
+                Write("SaveFile", SaveFile);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            Merge();
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            MessageBox.Show(this, "This program selects pages from a set of PDF files specified in an INI Selector File and merges them to an output file.", "PDF Select & Merge", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+            if (File.Exists(SaveFile)) {
+                Process.Start(SaveFile);
+            } else {
+                MessageBox.Show(this, "File has been removed.", "PDF Select & Merge", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            Process.Start("https://github.com/woborschilde/pdfsm/releases");
+        }
+        #endregion
+
+    }
+}
